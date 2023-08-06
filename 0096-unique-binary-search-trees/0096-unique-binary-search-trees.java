49429ff95783cd1599668f185de85b8799aa521a
Tanjul Sarathe <97596364+tanjul17@users.noreply.github.com>
2023-08-06 17:00:04 +0530

    Time: 0 ms (100.00%), Space: 39.4 MB (46.29%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.java
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.java
@@ -1,10 +1,18 @@
 class Solution {
     public int numTrees(int n) {
+        int dp[] = new int [n+1];
+        Arrays.fill(dp,-1);
+        return solve(n,dp);
+    }
+    public int solve(int n,int dp[]){
         if(n==0 || n==1) return 1;
         int ans = 0;
-        for(int i = 0;i<n;i++){
-            ans +=numTrees(i)*numTrees(n-i-1);
+        if(dp[n]!=-1){
+            return dp[n];
         }
-        return ans;
+        for(int i = 0;i<n;i++){
+            ans +=solve(i,dp)*solve(n-i-1,dp);
+        }
+        return dp[n]=ans;
     }
 }
