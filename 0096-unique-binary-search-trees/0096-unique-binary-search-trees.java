f8d6e88a677ad63afcf053a9911a5c2fc80a5a87
Tanjul Sarathe <97596364+tanjul17@users.noreply.github.com>
2023-08-06 14:39:40 +0530

    Time: 1985 ms (10.27%), Space: 39 MB (90.36%) - LeetHub

---
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int numTrees(int n) {
+        if(n==0 || n==1) return 1;
+        int ans = 0;
+        for(int i = 0;i<n;i++){
+            ans +=numTrees(i)*numTrees(n-i-1);
+        }
+        return ans;
+    }
+}
