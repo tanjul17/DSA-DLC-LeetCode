eaeea32a804d0701bb41f39645d6061e5e41139c
Tanjul Sarathe <97596364+tanjul17@users.noreply.github.com>
2023-12-21 17:16:55 +0530

    Time: 1 ms (89.31%) | Memory: 45 MB (5.88%) - LeetSync

---
+++ 152-maximum-product-subarray/maximum-product-subarray.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maxProduct(int[] nums) {
+        int res = Integer.MIN_VALUE, fp = 1, bp = 1;
+        for(int i=0;i<nums.length;i++){
+            fp *= nums[i];
+            bp *= nums[nums.length-1-i];
+            res = Math.max(res,Math.max(fp,bp));
+            if(fp == 0) fp = 1;
+            if(bp == 0) bp = 1;
+        }
+        return res;
+    }
+}
