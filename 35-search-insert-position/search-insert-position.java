bcb65d2cb596630315b52c489bff034436fe915d
Tanjul Sarathe <97596364+tanjul17@users.noreply.github.com>
2023-12-30 00:29:13 +0530

    Time: 0 ms (100.00%) | Memory: 42.6 MB (98.60%) - LeetSync

--- 35-search-insert-position/search-insert-position.java
+++ 35-search-insert-position/search-insert-position.java
@@ -1,27 +1,17 @@
 class Solution {
     public int searchInsert(int[] arr, int m) {
-        int s = 0, e = arr.length - 1, res = -1;
-
-        if (m < arr[0]) {
-            return 0;
-        }
-
-        if (m > arr[arr.length - 1]) {
-            return arr.length;
-        }
+        int s = 0, e = arr.length - 1, res = arr.length;
 
         while (s <= e) {
             int mid = s + (e - s) / 2;
 
-            if (arr[mid] == m) {
-                return mid;
-            } else if (arr[mid] > m) {
-                res = mid; // Update the result but continue searching in the left half
+            if (arr[mid] >= m) {
+                res = mid; 
                 e = mid - 1;
             } else {
                 s = mid + 1;
             }
         }
-        return res; // Return the final result
+        return res; 
     }
 }
