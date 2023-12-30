1addc87e1574f2a6a2382cf3a193329e9ed7115f
Tanjul Sarathe <97596364+tanjul17@users.noreply.github.com>
2023-12-30 19:09:32 +0530

    Time: 4 ms (43.02%) | Memory: 43.5 MB (20.75%) - LeetSync

--- 2025-redistribute-characters-to-make-all-strings-equal/redistribute-characters-to-make-all-strings-equal.java
+++ 2025-redistribute-characters-to-make-all-strings-equal/redistribute-characters-to-make-all-strings-equal.java
@@ -1,17 +1,17 @@
 
 class Solution {
     public boolean makeEqual(String[] words) {
-        HashMap<Character, Integer> map = new HashMap<>();
-
+        int [] hash = new int [26];
         for (String word : words) {
             char[] chars = word.toCharArray();
             for (char c : chars) {
-                map.put(c, map.getOrDefault(c, 0) + 1);
+                int i = (int)(c-'a');
+                hash[i] = hash[i]+1;
             }
         }
 
-        for (int c : map.values()) {
-            if (c % words.length != 0) {
+        for (int i=0;i<26;i++) {
+            if (hash[i] % words.length != 0) {
                 return false;
             }
         }
