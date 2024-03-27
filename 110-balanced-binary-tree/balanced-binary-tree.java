33f058cd32466b638f7dbe06ade57087951619e4
Tanjul Sarathe <97596364+tanjul17@users.noreply.github.com>
2024-03-27 15:47:17 +0530

    Time: 0 ms (100.00%) | Memory: 43.7 MB (97.55%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.java
+++ 110-balanced-binary-tree/balanced-binary-tree.java
@@ -1,20 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
     public boolean isBalanced(TreeNode root) {
-        if (root == null) {
-            return true; 
-        }
-        return Math.abs(height(root.left) - height(root.right)) <= 1 &&
-               isBalanced(root.left) &&
-               isBalanced(root.right);
+        return dfs(root)!=-1;
     }
-
-    public int height(TreeNode root) {
-        if (root == null) {
-            return 0; 
-        }
-        int l = height(root.left);
-        int r = height(root.right);
-
-        return Math.max(l,r) + 1;
+    public int dfs(TreeNode root){
+        if(root==null)return 0;
+        int l = dfs(root.left);
+        if(l==-1)return -1;
+        int r = dfs(root.right);
+        if(r==-1)return -1;
+        if(Math.abs(l-r)>1)return -1;
+        return Math.max(l,r)+1;
     }
 }
