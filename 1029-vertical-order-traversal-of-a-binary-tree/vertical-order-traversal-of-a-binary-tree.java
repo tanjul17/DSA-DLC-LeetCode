a86eff8d7ae65399652a95b374cd0a1a02e513a2
Tanjul Sarathe <97596364+tanjul17@users.noreply.github.com>
2024-05-06 17:27:47 +0530

    Time: 4 ms (43.09%) | Memory: 42.2 MB (82.67%) - LeetSync

---
+++ 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.java
@@ -0,0 +1,71 @@
+/*class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    
+    public TreeNode() {}
+    
+    public TreeNode(int val) {
+        this.val = val;
+    }
+    
+    public TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+*/
+
+class Tuple {
+    TreeNode node;
+    int row;
+    int col;
+
+    public Tuple(TreeNode _node, int _row, int _col) {
+        this.node = _node;
+        this.row = _row;
+        this.col = _col;
+    }
+}
+
+class Solution {
+    public List<List<Integer>> verticalTraversal(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        TreeMap<Integer, TreeMap<Integer, PriorityQueue<Integer>>> map = new TreeMap<>();
+        Queue<Tuple> q = new LinkedList<>();
+        q.offer(new Tuple(root, 0, 0));
+
+        while (!q.isEmpty()) {
+            Tuple tuple = q.poll();
+            TreeNode node = tuple.node;
+            int x = tuple.row;
+            int y = tuple.col;
+            
+            if (!map.containsKey(x)) {
+                map.put(x, new TreeMap<>());
+            }
+            if (!map.get(x).containsKey(y)) {
+                map.get(x).put(y, new PriorityQueue<>());
+            }
+            map.get(x).get(y).offer(node.val);
+            
+            if (node.left != null) {
+                q.offer(new Tuple(node.left, x - 1, y + 1));
+            }
+            if (node.right != null) {
+                q.offer(new Tuple(node.right, x + 1, y + 1));
+            }
+        }
+
+        for (TreeMap<Integer, PriorityQueue<Integer>> ys : map.values()) {
+            res.add(new ArrayList<>());
+            for (PriorityQueue<Integer> nodes : ys.values()) {
+                while (!nodes.isEmpty()) {
+                    res.get(res.size() - 1).add(nodes.poll());
+                }
+            }
+        }
+        return res;
+    }
+}
