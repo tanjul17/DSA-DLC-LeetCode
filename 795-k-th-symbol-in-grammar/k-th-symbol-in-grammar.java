237884c3a5f4021267b9b6ed983a8e83c2caf66d
Tanjul Sarathe <97596364+tanjul17@users.noreply.github.com>
2024-05-07 16:00:40 +0530

    Time: 0 ms (100.00%) | Memory: 39.7 MB (96.34%) - LeetSync

--- 795-k-th-symbol-in-grammar/k-th-symbol-in-grammar.java
+++ 795-k-th-symbol-in-grammar/k-th-symbol-in-grammar.java
@@ -2,7 +2,7 @@ class Solution {
     public int kthGrammar(int n, int k) {
         if(n==1&&k==1)return 0;
         if(n==2&&k==2)return 1;
-        int mid = (int)(Math.pow(2,n-1)/2);
+        int mid = (int)(Math.pow(2,n-2));
         if(k<=mid){
             return kthGrammar(n-1,k);
         }else{
