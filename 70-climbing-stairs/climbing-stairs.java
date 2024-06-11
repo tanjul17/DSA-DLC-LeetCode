54cd03e5c01f9d2fa6cb51a6ae8e0051ea16ff85
Tanjul Sarathe <97596364+tanjul17@users.noreply.github.com>
2024-06-11 12:20:36 +0530

    Time: 0 ms (100.00%) | Memory: 39.6 MB (98.04%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,11 +1,19 @@
 class Solution {
     public int climbStairs(int n) {
-        int dp[] = new int [n+1];
-        dp[0]=1;
-        for(int i=1;i<=n;i++){
-            if(i==1) dp[i] = dp[i-1]+0;
-            else dp[i] = dp[i-1]+dp[i-2];
-        }
+        //recurrsive
+        // if(n==0)return 1;
+        // if(n<0)return 0;
+        // return climbStairs(n-1)+climbStairs(n-2);
+        //Memo
+        int dp[]=new int [n+1];
+        Arrays.fill(dp,-1);
+       return solve(n,dp);
+    }
+    public int solve(int n, int dp[]){
+        if(n==0)return 1;
+        if(n<0)return 0;
+        if(dp[n]!=-1)return dp[n];
+        dp[n] = solve(n-1,dp)+solve(n-2,dp);
         return dp[n];
     }
 }
